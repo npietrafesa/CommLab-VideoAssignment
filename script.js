675368c42d51b325770a226678f57d3b44c891bc
Dev Kalavadiya <67102702+Dev-Kalavadia@users.noreply.github.com>
2022-04-25 13:31:16 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // Video Popup JavaScript 
 $(document).ready(function(){
+  // For loop that goes thhrough all the 4 timeline images, div's, Video frames and background by index
   for(let i = 1; i < 5; i++){
 
-  $("#img"+i).on('click', function(){
-    $("#"+i).fadeIn('slow');
+  $("#img"+i).on('click', function(){ // One image click on the timeline 
+    $("#"+i).fadeIn('slow'); // Fading in the i th video frame 
     return false;
   });
   
-  $("#bg"+i).on('click', function(){
-    $("#"+i).slideUp('slow');
+  $("#bg"+i).on('click', function(){ // One background click ater the image
+    $("#"+i).slideUp('slow'); // sliding the video up 
     return false;
   });
   
-   $('.close-btn').on('click', function(){
-     $("#"+i).fadeOut('slow');
+   $('.close-btn').on('click', function(){ // One background click on close buttom
+     $("#"+i).fadeOut('slow'); // fading the div out
       return false;
    });
   }
